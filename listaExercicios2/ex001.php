c4cc0fffc873588d29e399299f50e53e8f72854b
joaopedro3177 <joaopedrosimiao2008@gmail.com>
2025-09-16 21:50:59 -0300

    Initial commit

---
+++ listaExercicios2/ex001.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$cont = 0;
+$resp = 1;
+$num1 = 3;
+$num2 =9;
+
+
+while ($resp != 0 && $cont < 1) {
+	if ($num1 > $num2) {
+		echo("$num1 é maior que $num2");
+	}
+
+	else{
+		echo("$num2 é maior que $num1");
+	}
+	$cont++;
+}
+
+
+
+
+
+ ?>
